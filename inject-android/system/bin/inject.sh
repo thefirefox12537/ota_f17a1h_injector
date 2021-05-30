9f54ab6ba468b558a08017a8b02abfced8c30e04
thefirefox12537 <reinmclaren33@gmail.com>
2021-05-30 18:32:56 +0700

    Revision #1.1.1: See changelog on README.md

--- inject-android/system/bin/inject.sh
+++ inject-android/system/bin/inject.sh
@@ -9,7 +9,7 @@ set -e
 MAGISKDIR="/data/adb"
 MAGISK="$MAGISKDIR/magisk"
 MAGISK_MODULE="$MAGISKDIR/modules"
-dir_0="$(dirname "$(readlink -f "$0")")"
+basedir="$(dirname "$(readlink -f "$0")")"
 
 USAGE () {
     echo -e "Inject update.zip for Haier F17A1H
@@ -26,44 +26,45 @@ README () {
     echo -e "Untuk mengaktifkan Debugging USB pada Andromax Prime
 sebagai berikut:
 
-  a.)  Dial/Tekan *#*#83781#*#*
-  b.)  Slide 2 (debug & log).
-  c.)  Design For Test.
-  d.)  CMCC lalu tekan OK.
-  e.)  MTBF.
-  f.)  Lalu Tekan MTBF.
-  g.)  Please Wait.
-  h.)  Pilih dan tekan Confirm.
-  i.)  Kalau Sudah Lalu Mulai Ulang (restart HP).
-  j.)  Selamat USB Debug Telah Aktif.
+  *  Dial/Tekan *#*#83781#*#*
+  *  Slide 2 (debug & log).
+  *  Design For Test.
+  *  CMCC lalu tekan OK.
+  *  MTBF.
+  *  Lalu tekan MTBF.
+  *  Please Wait.
+  *  Pilih dan tekan Confirm.
+  *  Kalau sudah lalu Mulai Ulang (restart HP).
+  *  Selamat USB Debug telah aktif.
 
-  
+
 Jika masih tidak aktif, ada cara lain sebagai berikut:
 
-  a.)  Dial/Tekan *#*#257384061689#*#*
-  b.)  Aktifkan \"USB Debugging\".
-  c.)  Izinkan aktifkan USB Debugging pada popupnya.
+  *  Dial/Tekan *#*#257384061689#*#*
+  *  Aktifkan 'USB Debugging'.
+  *  Izinkan aktifkan USB Debugging pada popupnya.
 
 
 Tinggal jalankan skrip ini dengan membuka Terminal,
-maka akan muncul popup izinkan sambung USB Debugging di
-Andromax Prime.
+maka akan muncul popup izinkan sambung USB Debugging
+di Andromax Prime.
 
 
 Special thanks to:
    < Adi Subagja >
+   < Ahka >
    < dan developer-developer Andromax Prime >" | more
     exit 1
 }
 
 start-adb () {
     echo "Starting ADB services..."
-    $ADB start-server
+    adb start-server
 }
 
 kill-adb () {
     echo "Killing ADB services..."
-    $ADB kill-server
+    adb kill-server
 }
 
 pause () {
@@ -79,14 +80,34 @@ case $1 in
 esac
 
 id="$(id)"; id="${id#*=}"; id="${id%%\(*}"; id="${id%% *}"
-[ "$id" != "0" ] && \
-[ "$id" != "root" ] && {
+[ "$id" != "0" ] && {
     echo "This script only allow in root mode."
     exit 1
 }
 
-[ -e "$MAGISK" ] || {
+echo "+ OS Name: $(getprop ro.build.id)"
+echo "+ Host: $(getprop ro.product.brand) $(getprop ro.product.model)"
+echo "+ Android: $(getprop ro.build.version.release) (SDK $(getprop ro.build.version.sdk))"
+echo "+ Kernel: $(uname -sr)"
+echo "+ Architecture: $(uname -m)"
+echo "+ Magisk installed: $([ -e "$MAGISK" ] && echo YES || echo NO)"
+echo
+
+[[ $(getprop ro.build.version.release) < "4.4"* \
+|| $(getprop ro.build.version.sdk) -lt 19 ]] && {
+    echo "This script cannot be run in older Android version."
+    exit 1
+}
+[[ $(uname -sr) < "Linux 3"* ]] && {
+    echo "This script requirements Linux Kernel Version 3.0 later."
+    exit 1
+}
+[ ! -e "$MAGISK" ] && {
     echo "This script requires Magisk."
+    exit 1
+}
+[ ! -e /system/etc/permissions/android.hardware.usb.host.xml ] && {
+    echo "This script requirements your devices installed USB Host."
     exit 1
 }
 
@@ -97,7 +118,7 @@ id="$(id)"; id="${id#*=}"; id="${id%%\(*}"; id="${id%% *}"
         exit 1
     }
 
-    echo -ne "File yang dipilih: $(dirname $(readlink -f $1))/$(basename $1) \nAnda yakin? "
+    echo -ne "File yang dipilih: \n$(dirname "$(readlink -f "$1")")/$(basename "$1") \nAnda yakin? "
     while true
     do
         read -srn1 YN
@@ -116,7 +137,7 @@ echo -ne "NOTE:  Harap baca dahulu sebelum eksekusi
  *  Harap aktifkan USB Debugging terlebih dahulu sebelum
     mengeksekusi inject update.zip [ Untuk membaca cara
     mengaktifkan USB debugging, dengan mengetik ]:
-      $0 --readme
+       $0 --readme
  *  Apabila HP terpasang kartu SIM, skrip akan terotomatis
     mengaktifkan mode pesawat.
 "
@@ -124,13 +145,9 @@ pause
 
 ## Checking ADB programs
 echo "Checking ADB program..."
-ADB="adb"
-[ -x "$MAGISK_MODULE/adb-ndk/system/bin/adb" ] && \
-ADB="$MAGISK_MODULE/adb-ndk/system/bin/adb"
-sleep 1
-which $ADB  >/dev/null 2>&1 && \
+[ -e "$MAGISK_MODULE/adb-ndk/system/bin/adb" ] && \
 echo "ADB program was availabled on this device." || {
-    echo -e "ADB program cannot be found on this device. \nMake sure ADB and Fastboot program already installed."
+    echo -e "ADB program cannot be found on this device. \nMake sure ADB and Fastboot module already installed."
     exit 1
 }
 
@@ -140,34 +157,49 @@ start-adb
 ## Checking devices
 echo "Connecting to device..."
 sleep 1; echo "Please plug USB to your devices."
-$ADB wait-for-device
+adb wait-for-device
 echo "Connected."
+
+## Checking if your devices is F17A1H
+echo "Checking if your devices is F17A1H..."
+for a in ro.product.device ro.build.product
+do
+    for device in $(adb shell "getprop $a")
+    do [ "$device" != "grouper" ] && ERROR=1
+    done
+done
+adb shell "getprop ro.build.id" > /dev/nulll 2>&1 | grep "F17A1H" || set ERROR=1
+[[ $ERROR -eq 1 ]] && {
+    echo "MESSAGE_ERROR=Perangkat anda bukan Andromax Prime/Haier F17A1H"
+    exit 1
+}
 
 ## Activating airplane mode
 echo "Activating airplane mode..."
-$ADB shell "settings put global airplane_mode_on 1"
-$ADB shell "am broadcast -a android.intent.action.AIRPLANE_MODE"
+adb shell "settings put global airplane_mode_on 1"
+adb shell "am broadcast -a android.intent.action.AIRPLANE_MODE"
 
 ## Injecting file
 echo "Preparing version file $1 to injecting device..."
-$ADB push "$1" /sdcard/adupsfota/update.zip
+adb push "$1" /sdcard/adupsfota/update.zip
 echo "Checking file..."
 echo "Verifying file..."
 sleep 12
 
 ## Calling FOTA update
 echo "Checking updates..."
-$ADB shell "settings put global install_non_market_apps 1"
+adb shell "settings put global install_non_market_apps 1"
+adb shell "settings put secure install_non_market_apps 1"
 
 echo "Cleaning FOTA updates..."
-$ADB shell "pm clear com.smartfren.fota"
+adb shell "pm clear com.smartfren.fota"
 
 echo "Manipulating FOTA updates..."
-$ADB shell "monkey -p com.smartfren.fota 1"
-$ADB shell "am start -n com.smartfren.fota/com.adups.fota.FotaPopupUpateActivity"
-$ADB shell "input keyevent 20"
-$ADB shell "input keyevent 22"
-$ADB shell "input keyevent 23"
+adb shell "monkey -p com.smartfren.fota 1"
+adb shell "am start -n com.smartfren.fota/com.adups.fota.FotaPopupUpateActivity"
+adb shell "input keyevent 20"
+adb shell "input keyevent 22"
+adb shell "input keyevent 23"
 
 ## Confirmation
 echo -ne "Persetujuan pengguna
@@ -184,7 +216,7 @@ do
     echo
     case $YN in
         y | Y )  break ;;
-        n | N )  $ADB shell "rm /sdcard/adupsfota/update.zip"
+        n | N )  adb shell "rm /sdcard/adupsfota/update.zip"
                  kill-adb
                  exit 1
                  ;;
@@ -194,15 +226,15 @@ done
 
 ## Start updating
 echo "Updating..."
-$ADB shell "am start -n com.smartfren.fota/com.adups.fota.FotaInstallDialogActivity"
+adb shell "am start -n com.smartfren.fota/com.adups.fota.FotaInstallDialogActivity"
 COUNTER=0
 while :
 do
-    $ADB shell "input keyevent 20"
+    adb shell "input keyevent 20"
     (( COUNTER+=1 ))
     [[ $COUNTER -eq 20 ]] && break
 done
-$ADB shell "input keyevent 23"
+adb shell "input keyevent 23"
 sleep 1
 
 ## Complete
--- inject-linux.sh
+++ inject-linux.sh
@@ -6,7 +6,7 @@ set -e
 # File update.zip dan batch script inject root dari Adi Subagja
 # File bash script inject dari Faizal Hamzah
 
-dir_0="$(dirname "$(readlink -f "$0")")"
+basedir="$(dirname "$(readlink -f "$0")")"
 
 USAGE () {
     echo -e "Inject update.zip for Haier F17A1H
@@ -20,45 +20,61 @@ Additional arguments are maybe to know:
 }
 
 README () {
-    echo -e "Untuk mengaktifkan Debugging USB pada Andromax Prime
+    README="Untuk mengaktifkan Debugging USB pada Andromax Prime
 sebagai berikut:
 
-  a.)  Dial/Tekan *#*#83781#*#*
-  b.)  Slide 2 (debug & log).
-  c.)  Design For Test.
-  d.)  CMCC lalu tekan OK.
-  e.)  MTBF.
-  f.)  Lalu Tekan MTBF.
-  g.)  Please Wait.
-  h.)  Pilih dan tekan Confirm.
-  i.)  Kalau Sudah Lalu Mulai Ulang (restart HP).
-  j.)  Selamat USB Debug Telah Aktif.
+  *  Dial/Tekan *#*#83781#*#*
+  *  Slide 2 (debug & log).
+  *  Design For Test.
+  *  CMCC lalu tekan OK.
+  *  MTBF.
+  *  Lalu tekan MTBF.
+  *  Please Wait.
+  *  Pilih dan tekan Confirm.
+  *  Kalau sudah lalu Mulai Ulang (restart HP).
+  *  Selamat USB Debug telah aktif.
 
-  
+
 Jika masih tidak aktif, ada cara lain sebagai berikut:
 
-  a.)  Dial/Tekan *#*#257384061689#*#*
-  b.)  Aktifkan \"USB Debugging\".
-  c.)  Izinkan aktifkan USB Debugging pada popupnya.
+  *  Dial/Tekan *#*#257384061689#*#*
+  *  Aktifkan 'USB Debugging'.
+  *  Izinkan aktifkan USB Debugging pada popupnya.
 
 
 Tinggal jalankan skrip ini dengan membuka Terminal,
-maka akan muncul popup izinkan sambung USB Debugging di
-Andromax Prime.
+maka akan muncul popup izinkan sambung USB Debugging
+di Andromax Prime."
 
-
+    [ "$DIALOG" == "kdialog" ] && {
+        $DIALOG --title "Read-Me" --msgbox "$README"
+        $DIALOG --title "Read-Me" --msgbox "Special thanks to:
+   1.  Adi Subagja
+   2.  Ahka
+   3.  dan developer-developer Andromax Prime"
+    } || \
+    echo -e "$README\n\n
 Special thanks to:
    < Adi Subagja >
+   < Ahka >
    < dan developer-developer Andromax Prime >" | more
     exit 1
 }
 
 start-adb () {
+    [ "$DIALOG" == "kdialog" ] && \
+    { echo "Starting ADB services..."
+      $ADB start-server
+    } || \
     { $ADB start-server 2>&1
     } | $DIALOG --gauge "Starting ADB services..." 6 63 0
 }
 
 kill-adb () {
+    [ "$DIALOG" == "kdialog" ] && \
+    { echo "Killing ADB services..."
+      $ADB kill-server
+    } || \
     { echo 0; sleep 1
       $ADB kill-server 2>&1
       echo 100
@@ -67,41 +83,16 @@ kill-adb () {
 
 
 ## Set dialog screen program
-[ ! -x "$(which dialog)" ] && DIALOG="dialog" || \
-[ ! -x "$(which whiptail)" ] && DIALOG="whiptail" || {
-    id="$(id)"; id="${id#*=}"; id="${id%%\(*}"; id="${id%% *}"
-    [ "$id" != "0" ] && \
-    [ "$id" != "root" ] && {
-        which su  >/dev/null 2>&1    &&  SUDO_COMMAND="su -c "
-        which sudo  >/dev/null 2>&1  &&  SUDO_COMMAND="sudo "
-    }
-    [ -e /etc/os-release ] && . /etc/os-release || . /usr/lib/os-release
+for d in dialog whiptail kdialog
+do which $d  > /dev/null 2>&1 && DIALOG="$d"
+done
 
-    ## Debian and Ubuntu based distro
-    [ "$ID" == "debian" ] || [ "$ID_LIKE" == "debian" ] || [ "$ID_LIKE" == "ubuntu" ] \
-    && REPO_COMMANDS="apt-get install"
-
-    ## Fedora and RedHat based distro
-    [ "$ID" == "fedora" ] || [ "$ID_LIKE" == "fedora" ] || [[ "$ID_LIKE" == *"rhel"* ]] \
-    && REPO_COMMANDS="dnf install"
-
-    ## Arch Linux based distro
-    [ "$ID" == "arch" ] || [ "$ID_LIKE" == "arch" ] \
-    && REPO_COMMANDS="pacman -S"
-
-    ## SUSE based distro
-    [[ "$ID" == *"suse"* ]] || [[ "$ID_LIKE" == *"suse"* ]] \
-    && REPO_COMMANDS="zypper in"
-
+[ -z "$DIALOG" ] && {
     echo -e "This script requirements dialog box, one of which:
 
-   dialog\n   whiptail\n"
-    [[ $REPO_COMMANDS ]] && \
-    echo -e "Please install that with type:
-
-   $SUDO_COMMAND$REPO_COMMANDS dialog
-       or
-   $SUDO_COMMAND$REPO_COMMANDS whiptail\n"
+   dialog
+   whiptail
+   kdialog\n"
     exit 1
 }
 
@@ -118,14 +109,52 @@ esac
         exit 1
     }
 
-    ( $DIALOG                                 \
-      --yesno                                 \
-"Anda yakin? File yang dipilih:
+    [ -e /etc/os-release ] && . /etc/os-release || . /usr/lib/os-release
 
-$(dirname $(readlink -f $1))/$(basename $1)"  \
-     11 63                                    \
-      --yes-button 'Ya'                       \
-      --no-button 'Tidak'                     \
+    [[ "$ID" = "debian" || "$ID_LIKE" = "debian" || "$ID_LIKE" = "ubuntu" ]] && DIST_CORE="debian"
+    [[ "$ID" = "fedora" || "$ID_LIKE" = "fedora" || "$ID_LIKE" = *"rhel"* ]] && DIST_CORE="redhat"
+    [[ "$ID" = *"suse"* || "$ID_LIKE" = *"suse"* ]] && DIST_CORE="suse"
+    [[ "$ID" = "arch" || "$ID_LIKE" = "arch" ]] && DIST_CORE="archlinux"
+
+    [[ -f /sys/devices/virtual/dmi/id/product_name ]] && \
+        HOST=$(< /sys/devices/virtual/dmi/id/product_name)
+    [[ -f /sys/firmware/devicetree/base/model ]] && \
+        HOST=$(< /sys/firmware/devicetree/base/model)
+    [[ -f /tmp/sysinfo/model ]] && \
+        HOST=$(< /tmp/sysinfo/model)
+    [[ "$HOST" ]] && HOST="Undefined"
+
+    echo "+ OS Name: $NAME $VERSION"
+    echo "+ Host: $HOST"
+    echo "+ Kernel: $(uname -sr)"
+    echo "+ Architecture: $(uname -p)"
+    echo
+
+    [[ ! "$DIST_CORE" ]] && {
+        echo "This script cannot be run in this Linux distribution"
+        exit 1
+    }
+    [[ $(uname -sr) < "Linux 4.4"* ]] && {
+        echo "This script requirements Linux Kernel Version 4.4 later"
+        exit 1
+    }
+    [[ $(uname -p) != *"64" ]] && {
+        echo "This script requirements a 64-bit Operating System"
+        exit 1
+    }
+
+    for d in dialog whiptail
+    do [ "$DIALOG" == "$d" ] && YESNO_LABEL="--yes-button Ya --no-button Tidak"
+    done
+
+    [ "$DIALOG" == "kdialog" ] && YESNO_LABEL="--yes-label Ya --no-label Tidak"
+
+    ( $DIALOG                                       \
+      --yesno                                       \
+"Anda yakin? File yang dipilih:
+$(dirname "$(readlink -f "$1")")/$(basename "$1")"  \
+     9 63                                           \
+     $YESNO_LABEL                                   \
       3>&1 1>&2 2>&3
     ) || exit 1
 } || USAGE
@@ -137,20 +166,21 @@ $DIALOG                                                      \
 " *  Harap aktifkan USB Debugging terlebih dahulu sebelum
     mengeksekusi inject update.zip [ Untuk membaca cara
     mengaktifkan USB debugging, dengan mengetik ]:
-      $0 --readme
+       $0 --readme
  *  Apabila HP terpasang kartu SIM, skrip akan terotomatis
     mengaktifkan mode pesawat." 11 63
 
 ## Checking ADB programs
-ADB="adb"
+ADB=adb
+[ "$DIALOG" == "kdialog" ] && \
+{ echo "Checking ADB program..."
+  [ -e "$basedir/adb" ] && ADB="./adb"
+  which $ADB  >/dev/null 2>&1 || ERROR=1
+} || \
 { sleep 0.2; echo 10
-  for a in "$dir_0" "$dir_0/bin"
-  do
-      [ -x "$a/adb" ] && \
-      ADB="$a/adb"
-  done
+  [ -e "$basedir/adb" ] && ADB="./adb"
   sleep 0.5; echo 50
-  which $ADB >/dev/null 2>&1 || ERROR=1
+  which $ADB  >/dev/null 2>&1 || ERROR=1
   sleep 0.7; echo 90
   sleep 0.1; echo 100
 } | $DIALOG --gauge "Checking ADB program..." 6 63 0
@@ -158,36 +188,97 @@ ADB="adb"
 ## Downloading ADB programs if not exist
 while true
 do
-    [[ $ERROR -eq 1 ]] && \
-    { echo 0; sleep 2
-      echo 20; sleep 1
-      wget -qO "/var/tmp/android-platform-tools.zip" \
-               https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-      echo 60; sleep 0.5
-      unzip -qo "$ZIP" platform-tools/adb \
-            -d "/var/tmp/"
-      echo 75; sleep 1
-      mv "/var/tmp/platform-tools/adb" "$dir_0/"  >/dev/null 2>&1
-      echo 80; sleep 1
-      rm -rf "/var/tmp/platform-tools"  >/dev/null 2>&1
-      rm "$ZIP"  >/dev/null 2>&1
-      echo 100; sleep 2
-      ADB="$dir_0/adb"
-    } | $DIALOG --gauge "Downloading ADB program..." 8 63
-    which $ADB >/dev/null 2>&1 && break
+    [ "$DIALOG" == "kdialog" ] && \
+    { [[ $ERROR -eq 1 ]] && \
+      { echo "Downloading Android SDK Platform Tools..."
+        wget -qO "/var/tmp/android-platform-tools.zip" \
+                 https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+        echo "Extracting Android SDK Platform Tools..."
+        unzip -qo "/var/tmp/android-platform-tools.zip" platform-tools/adb \
+              -d "/var/tmp/"
+        mv "/var/tmp/platform-tools/adb" "$basedir/"  >/dev/null 2>&1
+        rm -rf "/var/tmp/platform-tools"  >/dev/null 2>&1
+        rm "/var/tmp/android-platform-tools.zip"  >/dev/null 2>&1
+        ADB="./adb"
+        SUCCESS="1"
+      } || echo "ADB program was availabled on the computer or this folder."
+      [[ "$SUCCESS" ]] && echo "ADB program was successfully placed."
+    } || \
+    { [[ $ERROR -eq 1 ]] && \
+      { echo 0; sleep 2
+        echo 20; sleep 1
+        wget -qO "/var/tmp/android-platform-tools.zip" \
+                 https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+        echo 60; sleep 0.5
+      } | $DIALOG --gauge "Downloading Android SDK Platform Tools..." 8 63 0
+      { unzip -qo "/var/tmp/android-platform-tools.zip" platform-tools/adb \
+              -d "/var/tmp/"
+        echo 75; sleep 1
+        mv "/var/tmp/platform-tools/adb" "$basedir/"  >/dev/null 2>&1
+        echo 80; sleep 1
+        rm -rf "/var/tmp/platform-tools"  >/dev/null 2>&1
+        rm "/var/tmp/android-platform-tools.zip"  >/dev/null 2>&1
+        ADB="./adb"
+        echo 100; sleep 2
+      } | $DIALOG --gauge "Extracting Android SDK Platform Tools..." 8 63 60
+    }
+    which $ADB  >/dev/null 2>&1 && break
 done
 
 ## Starting ADB service
 start-adb
 
 ## Checking devices
+[ "$DIALOG" == "kdialog" ] && echo "Connecting to device..." || \
 { sleep 1; echo 50
 } | $DIALOG --gauge "Connecting to device..." 6 63 0
+
+[ "$DIALOG" == "kdialog" ] && \
+{ sleep 1
+  echo "Please plug USB to your devices."
+  $ADB wait-for-device
+} || \
 { $ADB wait-for-device 2>&1
   echo 100
 } | $DIALOG --gauge "Please plug USB to your devices." 6 63 50
 
+## Checking if your devices is F17A1H
+[ "$DIALOG" == "kdialog" ] && \
+{ echo "Checking if your devices is F17A1H..."
+  for a in ro.product.device ro.build.product
+  do
+      for device in $($ADB shell "getprop $a")
+      do [ "$device" != "grouper" ] && ERROR=2
+      done
+  done
+  $ADB shell "getprop ro.build.id" > /dev/nulll 2>&1 | grep "F17A1H" || set ERROR=2
+  [[ $ERROR -eq 2 ]] && {
+      echo "MESSAGE_ERROR=Perangkat anda bukan Andromax Prime/Haier F17A1H"
+      exit 1
+  }
+} || {
+    { for a in ro.product.device ro.build.product
+      do
+          for device in $($ADB shell "getprop $a")
+          do [ "$device" != "grouper" ] && ERROR=2
+          done
+      done
+      echo 50; sleep 0.3
+      $ADB shell "getprop ro.build.id" 2>&1 | grep "F17A1H" || set ERROR=2
+      echo 100
+    } | $DIALOG --gauge "Checking if your devices is F17A1H..." 6 63 0
+    [[ $ERROR -eq 2 ]] && {
+        $DIALOG --msgbox "\n     Perangkat anda bukan Andromax Prime/Haier F17A1H" 8 63
+        exit 1
+    }
+}
+
 ## Activating airplane mode
+[ "$DIALOG" == "kdialog" ] && \
+{ echo "Activating airplane mode..."
+  $ADB shell "settings put global airplane_mode_on 1"
+  $ADB shell "am broadcast -a android.intent.action.AIRPLANE_MODE"
+} || \
 { $ADB shell "settings put global airplane_mode_on 1" 2>&1
   echo 50
   $ADB shell "am broadcast -a android.intent.action.AIRPLANE_MODE" 2>&1
@@ -195,8 +286,14 @@ start-adb
 } | $DIALOG --gauge "Activating airplane mode..." 6 63 0
 
 ## Injecting file
+[ "$DIALOG" == "kdialog" ] || \
 { sleep 2; echo 100
 } | $DIALOG --gauge "Preparing version file $1 to injecting device..." 6 63 0
+
+[ "$DIALOG" == "kdialog" ] && \
+{ echo "Preparing version file $1 to injecting device..."
+  $ADB push "$1" /sdcard/adupsfota/update.zip
+} || \
 { COUNTER=0
   while :
   do
@@ -216,8 +313,15 @@ eof
   echo 99; sleep 0.1
   echo 100
 } | $DIALOG --gauge "Injecting..." 6 63 0
+
+[ "$DIALOG" == "kdialog" ] && echo "Checking file..." || \
 { sleep 2; echo 100
 } | $DIALOG --gauge "Checking file..." 6 63 0
+
+[ "$DIALOG" == "kdialog" ] && \
+{ echo "Verifying file..."
+  sleep 12
+} || \
 { COUNTER=0
   while :
   do
@@ -234,11 +338,30 @@ eof
 } | $DIALOG --gauge "Verifying file..." 6 63 0
 
 ## Calling FOTA update
+[ "$DIALOG" == "kdialog" ] && \
+{ echo "Checking updates..."
+  $ADB shell "settings put global install_non_market_apps 1"
+  $ADB shell "settings put secure install_non_market_apps 1"
+} || \
 { $ADB shell "settings put global install_non_market_apps 1" 2>&1
+  $ADB shell "settings put secure install_non_market_apps 1" 2>&1
 } | $DIALOG --gauge "Checking updates..." 6 63 0
 
+[ "$DIALOG" == "kdialog" ] && \
+{ echo "Cleaning FOTA updates..."
+  $ADB shell "pm clear com.smartfren.fota"
+} || \
 { $ADB shell "pm clear com.smartfren.fota" 2>&1
 } | $DIALOG --gauge "Cleaning FOTA updates..." 6 63 0
+
+[ "$DIALOG" == "kdialog" ] && \
+{ echo "Manipulating FOTA updates..."
+  $ADB shell "monkey -p com.smartfren.fota 1"
+  $ADB shell "am start -n com.smartfren.fota/com.adups.fota.FotaPopupUpateActivity"
+  $ADB shell "input keyevent 20"
+  $ADB shell "input keyevent 22"
+  $ADB shell "input keyevent 23"
+} || \
 { $ADB shell "monkey -p com.smartfren.fota 1" 2>&1
   echo 27
   $ADB shell "am start -n com.smartfren.fota/com.adups.fota.FotaPopupUpateActivity" 2>&1
@@ -251,6 +374,12 @@ eof
   echo 100
 } | $DIALOG --gauge "Manipulating FOTA updates..." 6 63 0
 
+for d in dialog whiptail
+do [ "$DIALOG" == "$d" ] && YESNO_LABEL="--yes-button Lanjutkan --no-button Batal"
+done
+
+[ "$DIALOG" == "kdialog" ] && YESNO_LABEL="--yes-label Lanjutkan --no-label Batal"
+
 ## Confirmation
 ( $DIALOG                                                    \
   --title "Persetujuan pengguna"                             \
@@ -259,8 +388,7 @@ eof
 jawab pembuat file ini serta tidak ada kaitannya dengan
 pihak manapun. Untuk lebih aman tanpa resiko, dianjurkan
 update secara daring melalui updater resmi." 10 63           \
-  --yes-button 'Lanjutkan'                                   \
-  --no-button 'Batal'                                        \
+  $YESNO_LABEL                                               \
   3>&1 1>&2 2>&3
 ) || \
 {
@@ -270,6 +398,19 @@ update secara daring melalui updater resmi." 10 63           \
 }
 
 ## Start updating
+[ "$DIALOG" == "kdialog" ] && \
+{ echo "Updating..."
+  $ADB shell "am start -n com.smartfren.fota/com.adups.fota.FotaInstallDialogActivity"
+  COUNTER=0
+  while :
+  do
+      $ADB shell "input keyevent 20" 2>&1
+      (( COUNTER+=1 ))
+      [[ $COUNTER -eq 20 ]] && break
+  done
+  $ADB shell "input keyevent 23"
+  sleep 1
+} || \
 { $ADB shell "am start -n com.smartfren.fota/com.adups.fota.FotaInstallDialogActivity" 2>&1
   COUNTER=0
   while :
@@ -290,6 +431,9 @@ eof
 } | $DIALOG --gauge "Updating..." 6 63 0
 
 ## Complete
+[[ "$DIALOG" == "kdialog" ]] && \
+$DIALOG --msgbox "Proses telah selesai" || \
 $DIALOG --msgbox "\n           Proses telah selesai" 8 48
+
 kill-adb
 exit 0
