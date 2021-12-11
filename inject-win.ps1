b76dc3543ee24d2af0a0c92eb796d2220c537eed
The Firefox Foundation <reinmclaren33@gmail.com>
2021-12-12 04:32:43 +0700

    Update inject-win.ps1

--- inject-win.ps1
+++ inject-win.ps1
@@ -9,10 +9,10 @@ if([Environment]::OSVersion.Version -lt (New-Object Version 6,1))
 {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}
 
 Invoke-WebRequest `
--uri http://github.com/thefirefox12537/ota_f17a1h_injector/releases/latest/download/$RunScript `
+-uri http://github.com/thefirefox12537/ota_f17a1h_injector/raw/main/$RunScript `
 -OutFile "${env:tmp}\$RunScript"
 
-& "${env:tmp}\$RunScript" @args
+& "${env:tmp}\$RunScript" $args
 Remove-Item -LiteralPath "${env:tmp}\$RunScript"
 
 exit 0
