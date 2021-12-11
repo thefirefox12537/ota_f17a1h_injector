3d5639ffc437d5e3cd343ca1c95a7be61b8c830c
The Firefox Foundation <reinmclaren33@gmail.com>
2021-12-12 06:22:42 +0700

    Update inject-win.ps1

--- inject-win.ps1
+++ inject-win.ps1
@@ -1,18 +1,10 @@
 #requires -version 4
 
-if ($IsLinux -or $IsmacOS) `
-{Write-Error "This script requires Microsoft Windows Operating System."; exit 1}
+if ($IsLinux -or $IsmacOS) {Write-Error "This script requires Microsoft Windows Operating System."; return 1}
 
 $ProgressPreference = 'SilentlyContinue'
 $RunScript = (Split-Path -Leaf $MyInvocation.MyCommand.Definition).Replace('.ps1','.bat')
-if([Environment]::OSVersion.Version -lt (New-Object Version 6,1))
-{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}
-
-Invoke-WebRequest `
--uri http://github.com/thefirefox12537/ota_f17a1h_injector/raw/main/$RunScript `
--OutFile "${env:tmp}\$RunScript"
-
+if ([Environment]::OSVersion.Version -lt (New-Object Version 6,1)) {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}
+(New-Object Net.WebClient).DownloadFile("http://github.com/thefirefox12537/ota_f17a1h_injector/raw/main/$RunScript","${env:tmp}\$RunScript")
 & "${env:tmp}\$RunScript" $args
 Remove-Item -LiteralPath "${env:tmp}\$RunScript"
-
-exit 0
