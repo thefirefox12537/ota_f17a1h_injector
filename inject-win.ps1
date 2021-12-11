b57da8b26c13f8b69617f25dda09e4846c2b25be
The Firefox Foundation <reinmclaren33@gmail.com>
2021-12-12 06:26:08 +0700

    Update inject-win.ps1

--- inject-win.ps1
+++ inject-win.ps1
@@ -5,6 +5,6 @@ if ($IsLinux -or $IsmacOS) {Write-Error "This script requires Microsoft Windows 
 $ProgressPreference = 'SilentlyContinue'
 $RunScript = (Split-Path -Leaf $MyInvocation.MyCommand.Definition).Replace('.ps1','.bat')
 if ([Environment]::OSVersion.Version -lt (New-Object Version 6,1)) {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}
-(New-Object Net.WebClient).DownloadFile("http://github.com/thefirefox12537/ota_f17a1h_injector/raw/main/$RunScript","${env:tmp}\$RunScript")
+(New-Object Net.WebClient).DownloadFile("http://github.com/thefirefox12537/ota_f17a1h_injector/raw/main/$RunScript", "${env:tmp}\$RunScript")
 & "${env:tmp}\$RunScript" $args
 Remove-Item -LiteralPath "${env:tmp}\$RunScript"
