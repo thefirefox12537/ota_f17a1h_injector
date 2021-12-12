026a9563a767e102ad65be06f4c5b15daa123235
The Firefox Foundation <reinmclaren33@gmail.com>
2021-12-12 07:32:18 +0700

    New release. See changelog 1.4.0: Revision #1

--- inject-win.ps1
+++ inject-win.ps1
@@ -3,11 +3,6 @@ if ($IsLinux -or $IsmacOS) {Write-Error "This script requires Microsoft Windows 
 if ([Environment]::OSVersion.Version -lt (New-Object Version 6,1)) {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}
 
 $ErrorActionPreference = 'Stop'
-$Repository = 'thefirefox12537/ota_f17a1h_injector'
-(New-Object Net.WebClient).DownloadFile(
-"http://github.com/$Repository/raw/main/inject-win.bat",
-"$env:tmp\inject-win.bat"
-)
-
+(New-Object Net.WebClient).DownloadFile("http://github.com/thefirefox12537/ota_f17a1h_injector/raw/main/inject-win.bat", "$env:tmp\inject-win.bat")
 & "$env:tmp\inject-win.bat" @args
 Remove-Item -LiteralPath "$env:tmp\inject-win.bat"
